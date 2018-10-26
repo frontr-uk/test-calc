784796f276af60dfbb10ef53174412e53f3fcbb3
Front End <chris@frontr.co.uk>
2018-10-26 12:02:52 +0100

    calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  form: FormGroup;
+  amount = 0;
+  total = 0;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      search: this.amount
+    });
+    this.form.valueChanges.subscribe((values) => {
+      this.updateTotal();
+    });
+  }
+
+  updateTotal() {
+    if(this.form.controls.search.value <= 1000) {
+      this.total = this.form.controls.search.value * 1 / 100;
+    }
+    if(this.form.controls.search.value > 1000 && this.form.controls.search.value <= 5000) {
+      this.total = this.form.controls.search.value * 2 / 100;
+    }
+    if(this.form.controls.search.value > 5000){
+      this.total = this.form.controls.search.value * 3 / 100;
+    }
+  }
+
 }
